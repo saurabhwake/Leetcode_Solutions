771ab6f67b4664ee17bf139ad2d6dad23ae4673e
Saurabh Wake <156418490+saurabhwake@users.noreply.github.com>
2025-05-12 08:21:21 +0530

    Time: 7 ms (44.17%), Space: 40.3 MB (64.16%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,12 +1,9 @@
-class Solution {
-    public int fib(int n) {
-        if (n<=1){
+class Solution{
+    public static int fib(int n){
+        if(n<=1){
             return n;
-        } else{
+        }else {
             return fib(n-1)+fib(n-2);
         }
-        
-    
-        
     }
 }
